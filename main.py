1215e2c2e11aa2290e0a26d3375a126506b70628
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2023-10-16 14:10:46 +0200

    last commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from elasticsearch import Elasticsearch
 
-ES_HOST_1 = "https://localhost:9200" # Host expose from a kubernetes proxy port-forward
-ES_HOST_2 = "https://minikube.io:443/elastic" # Host expose from a ingress controller
-ES_HOST_3 = "https://avib-elastic:9200" # Host expose from a kubernetes proxy port-forward but using a domain alias included inside elastic certs
-ES_HOST_4 = "https://avispe.edv.uniovi.es:443/kubernetes/elastic" # Host expose from a ingress controller
+ES_HOST_1 = "https://localhost:9200" # Local elasticsearch expose from a kubernetes proxy port-forward
+ES_HOST_2 = "https://minikube.io:443/elastic" # Local elasticsearch expose from a ingress controller
+ES_HOST_3 = "https://avib-elastic:9200" # Local elasticsearch expose from a kubernetes proxy port-forward but using a domain alias included inside elastic certs
+ES_HOST_4 = "https://avispe.edv.uniovi.es:443/kubernetes/elastic" # AVIB elasticsearch expose from a ingress controller
 ES_USERNAME = "elastic"
 ES_PASSWORD = "password"
 ES_CA_CERTS_01 = "ca-elastic-local.crt"
